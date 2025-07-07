6f3dc2cefd93cc2b98fa16ac7990f5ba55268aaa
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-07 17:40:07 +0530

    Update packed_array.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/PACKED_ARRAY/packed_array.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/PACKED_ARRAY/packed_array.v
@@ -1,1 +1,13 @@
-
+module packed_array;
+  logic [7:0] byte_data;
+  
+  initial begin
+    byte_data = 8'b10101100;
+  
+    $display("Full byte:%b", byte_data);
+    $display("MSB bit:%b", byte_data[7]);
+    $display("LSB byte:%b", byte_data[0]);
+    $display("[6:3] bit:%b", byte_data[6:3]);
+  end
+endmodule
+    
