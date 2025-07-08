77be94f66405036274d7a58639d9639d7f4a7002
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-08 23:18:42 +0530

    Create dynamic_array_delete.sv

---
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/DYNAMIC_ARRAYS/dynamic_array_delete.sv
@@ -0,0 +1,15 @@
+module dynamic_array_delete;
+  int arr[];
+  
+  initial begin
+    arr = new[3];
+    arr = '{10,20,30};
+    
+    $display("Before delete,size = %0d", arr.size());
+    $display("arr = %p", arr);
+    
+    arr.delete();
+    
+    $display("After delete, size = %0d",arr.size());
+  end
+endmodule
