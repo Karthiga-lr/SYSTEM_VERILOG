9110621e95031bd02a1e9a21ca6748fdb23d2726
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:52:22 +0530

    Update queue_delete.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/queue_delete.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/queue_delete.sv
@@ -1,1 +1,17 @@
+module delete;
+  int q[$];
+  string q1[$:4];
 
+  initial begin
+    
+    q = {5,3,8,9,2,6,0,1};
+    q1 = {"X", "Y", "Z"};
+    
+    q.delete(4);
+    $display("q = %p", q);
+    
+    q1.delete(1);
+    $display("q1 = %p", q1);
+    
+  end
+endmodule
