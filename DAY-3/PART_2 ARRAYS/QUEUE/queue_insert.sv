0d8eeb28dd07fc8e752b680bd49f3da8fc75c07d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:47:49 +0530

    Update queue_insert.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/queue_insert.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/queue_insert.sv
@@ -1,1 +1,15 @@
-
+module insert;
+  int q[$];    //unbounded
+  int q1[$:4];  //bounded
+  
+  initial begin
+    q = {4,3,7,8,6,1,0};
+    q1 = {3,4,5,7};
+    
+    q.insert(4,9);
+    $display("q = %p",q);
+    q1.insert(3,6);
+    $display("q1 = %p",q1);
+    
+  end
+endmodule
