b4764a61fecb6feaca33ec872bd174318a78273a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:11:40 +0530

    Update push_back.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/push_back.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/push_back.sv
@@ -1,1 +1,15 @@
-
+module push_back;
+  int q[$];    //unbounded
+  int q1[$:4];  //bounded
+  
+  initial begin
+    q = {4,3,7,8,6,1,0};
+    q1 = {3,4,5,6};
+    
+    q.push_back(1);
+    $display("q = %p",q);
+    q1.push_back(7);
+    $display("q1 = %p",q1);
+    
+  end
+endmodule
