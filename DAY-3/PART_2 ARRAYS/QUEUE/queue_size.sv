08cd321993a834da767630317920a7a06207d6da
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:58:18 +0530

    Update queue_size.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/queue_size.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/queue_size.sv
@@ -1,1 +1,14 @@
+module size;
+  int q[$]; //unbounded
+  string q1[$:4]; //bounded
 
+  initial begin
+    
+    q = {5,3,8,9,2,6,0,1};
+    q1 = {"X", "Y", "Z"};
+
+    $display("q = %p", q.size());
+    $display("q1 = %p", q1.size());
+    
+  end
+endmodule
