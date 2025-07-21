37b1c548acb569f2d91cf8e15e00dcffbc1bdf2d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 23:05:20 +0530

    Update associative_array_size.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_size.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_size.v
@@ -1,1 +1,11 @@
+module assoc_array_size;
+  int arr[string];  
 
+  initial begin
+    arr["apple"] = 10;
+    arr["banana"] = 20;
+    arr["cherry"] = 30;
+
+    $display("Associative array size = %0d", arr.size());
+  end
+endmodule
