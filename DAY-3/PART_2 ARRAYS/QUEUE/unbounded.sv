f25fae39a1fd259cc7b20003a0e0d33e6cdf74e2
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 22:33:28 +0530

    Update unbounded.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/unbounded.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/unbounded.sv
@@ -1,1 +1,8 @@
-
+module unbounded_type;
+  int queue[$]; //unbounded
+  initial begin
+    queue = '{4,5,6,7,8};
+    //$display("queue = '[4,5,6,7,8]");
+    $display("queue = %p",queue);
+  end
+endmodule
