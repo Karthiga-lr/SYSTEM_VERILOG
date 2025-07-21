13ea81dcd5c790adcb7990beee3d1a8e68e5b72d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 16:54:32 +0530

    Update associative_array_delete.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_delete.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_delete.v
@@ -1,1 +1,14 @@
-
+module assoc_array_delete;
+  int arr[];
+  
+  initial begin
+    arr = '{10,20,30};
+    
+    $display("Before delete,size = %0d", arr.size());
+    $display("arr = %p", arr);
+    
+    arr.delete();
+    
+    $display("After delete, size = %0d",arr.size());
+  end
+endmodule
