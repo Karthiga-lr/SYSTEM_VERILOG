5d6f43cc74b050df101536176224aa1106a5652f
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:06:19 +0530

    Update push_front.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/push_front.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/push_front.sv
@@ -1,1 +1,16 @@
-
+module push_front;
+  int q[$];    //unbounded
+  int q1[$:4];  //bounded
+  
+  initial begin
+    q = {4,3,7,8,6,1,0};
+    q1 = {3,4,5,6};
+    
+    q.push_front(3);
+    $display("q = %p",q);
+    q1.push_front(2);
+    $display("q1 = %p",q1);
+    
+  end
+endmodule
+    
