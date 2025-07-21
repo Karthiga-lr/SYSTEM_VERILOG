a6caf4beb4ffbcb1c453339b3d6e7cdcb6031eee
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-21 15:26:10 +0530

    Update pop_front.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/pop_front.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/pop_front.sv
@@ -1,1 +1,15 @@
-
+module pop_front;
+  int q[$];    //unbounded
+  int q1[$:4];  //bounded
+  
+  initial begin
+    q = {4,3,7,8,6,1,0};
+    q1 = {3,4,5,6,9};
+    
+    q.pop_front();
+    $display("q = %p",q);
+    q1.pop_front();
+    $display("q1 = %p",q1);
+    
+  end
+endmodule
