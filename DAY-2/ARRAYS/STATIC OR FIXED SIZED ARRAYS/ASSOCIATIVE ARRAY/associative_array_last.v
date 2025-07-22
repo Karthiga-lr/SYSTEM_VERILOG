512edd147bc0a9a0d918711c319c431a5b0b2135
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-22 17:36:33 +0530

    Update associative_array_last.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_last.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_last.v
@@ -1,1 +1,14 @@
-
+module assoc_array_last;
+  int value[int];
+  int detect_last;
+  initial 
+    begin
+      value[1] = 10;
+      value[2] = 20;
+      value[3] = 30;
+      value.last(detect_last);
+      $display("detect the last = %0d",detect_last);
+      $display("Detect the last value = %0d",value[detect_last]);
+      
+    end
+endmodule
