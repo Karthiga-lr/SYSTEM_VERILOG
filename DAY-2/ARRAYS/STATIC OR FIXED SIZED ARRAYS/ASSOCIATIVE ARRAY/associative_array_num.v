29acbcdd56f300ff0b8bf6028d3c7c280628eb2e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-22 18:03:29 +0530

    Update associative_array_num.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_num.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_num.v
@@ -1,1 +1,13 @@
-
+module assoc_array_num;
+  int value[int];
+  int count;
+  initial begin
+    value [1] = 10;
+    value [2] = 20;
+    value [3] = 30;
+    count = value.num();
+    $display("The numbers is: %0d",count);
+    foreach(value[i])
+      $display("Number of values : %0d", i, value[i]);
+  end
+endmodule
