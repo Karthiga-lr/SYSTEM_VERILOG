cefbfe6fed200dc3ca09c6d20c59da69c4d07321
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-22 17:45:10 +0530

    Update associative_array_next.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_next.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_next.v
@@ -1,1 +1,15 @@
-
+module assoc_array_next;
+  int value[int];
+  int detect_next; // this is the key
+  initial 
+    begin
+      value[1] = 10;
+      value[2] = 20;
+      value[3] = 30;
+      detect_next = 1; //It is used to detect the 2nd value by inserting detect_next = 1
+      value.next(detect_next);
+      $display("detect the next = %0d",detect_next);
+      $display("Detect the next value = %0d",value[detect_next]);
+      
+    end
+endmodule
