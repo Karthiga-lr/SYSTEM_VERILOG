4b47e3443fa1a5844d12332aee92ce51a3abd70c
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-22 17:16:24 +0530

    Update associative_array_first.v

--- DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_first.v
+++ DAY-2/ARRAYS/STATIC OR FIXED SIZED ARRAYS/ASSOCIATIVE ARRAY/associative_array_first.v
@@ -1,1 +1,17 @@
-module assoc_array_find;
+module assoc_array_first;
+  int arr[string];  
+  string key;   //key is important for associative_array first to get the value
+ 
+  initial begin
+    arr["apple"] = 10;
+    arr["banana"] = 20;
+    arr["cherry"] = 30;
+    
+    if(arr.first(key))begin
+      $display("First key in the associative array = %s", key);
+      $display("Value of array is = %0d",arr[key]);
+      end else begin
+      $display("Array is empty"); 
+      end
+  end
+endmodule
