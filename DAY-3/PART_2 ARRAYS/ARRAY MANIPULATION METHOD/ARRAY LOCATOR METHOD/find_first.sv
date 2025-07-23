58819690ad1161ac59b1db1c83856fa670f3a3b9
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-23 11:41:16 +0530

    Update find_first.sv

--- DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY LOCATOR METHOD/find_first.sv
+++ DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY LOCATOR METHOD/find_first.sv
@@ -1,1 +1,11 @@
-
+module find_first_method;
+  int data[$];
+  int index;
+  
+  initial begin
+    data = {11,8,10,12,14,16};
+    $display("find the first index: %p", data.find_first(index) with (index <= 13));
+  end 
+endmodule
+             
+//syntax array.find_first(check) with (check < "yelahanka" & check >= "newton" );
