1065da19825b102eec2ead437b196560d7d246bb
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-23 10:57:13 +0530

    Update find_index.sv

--- DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY LOCATOR METHOD/find_index.sv
+++ DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY LOCATOR METHOD/find_index.sv
@@ -1,1 +1,15 @@
+module find_index;
+  int data[int];
+  int index;
+  
+  initial begin
+    data[0] = 10;
+    data[1] = 20;
+    data[2] = 30;
+    
+    $display("Find the index: %p", data.find_index(y) with (y == 20));
+  end
+endmodule
 
+
+//array.find_index(check) with (check =="yelahanka");
