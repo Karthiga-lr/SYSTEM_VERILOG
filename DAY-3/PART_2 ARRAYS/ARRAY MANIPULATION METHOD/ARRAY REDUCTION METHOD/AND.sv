22165951b81e3ec0ed9c373a56145f93c1c42318
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-24 11:49:08 +0530

    Update AND.sv

--- DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY REDUCTION METHOD/AND.sv
+++ DAY-3/PART_2 ARRAYS/ARRAY MANIPULATION METHOD/ARRAY REDUCTION METHOD/AND.sv
@@ -1,1 +1,9 @@
-
+module AND_of_num;
+  int num[$];  
+  
+  initial begin
+    num = {15,5,8,10}; //Bitwise AND 1111,0101,1000,1010
+    
+    $display("AND of numbers: 0X%0h",num.and());
+  end 
+endmodule
