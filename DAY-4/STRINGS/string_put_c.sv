25791ce600ce767813e4c7483d551ca64710d13c
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-25 14:52:02 +0530

    Update string_put_c.sv

--- DAY-4/STRINGS/string_put_c.sv
+++ DAY-4/STRINGS/string_put_c.sv
@@ -1,1 +1,16 @@
+module str_putchar;
+  string s1, s2, s3;
+
+  initial begin
+    s1 = "SAM";
+    s2 = "LRKS";
+    s3 = "KAR";
+    s1.putc(2, "S");
+    $display("put character in str1: %0s", s1);
+    s2.putc(3, "C");
+    $display("put character in str2: %0s", s2);
+    s3.putc(0, "A");       
+    $display("put character in str3: %0s", s3);
+  end
+endmodule
 
