5c171c0fa9d8bb43d68e83373cd93090d70a7c18
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-25 12:47:58 +0530

    Update String_length.sv

--- DAY-4/STRINGS/String_length.sv
+++ DAY-4/STRINGS/String_length.sv
@@ -1,1 +1,10 @@
-
+module str_length;
+  string str_1,str_2,str_3;
+  
+  initial begin 
+    str_1 = "LRK";
+    str_2 = "LRKS";
+    str_3 = "154";
+    $display("%0d",str_2.len());
+      end
+endmodule
