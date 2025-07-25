2fb029b75cf6ab208dcbc09c9c4b4095c9717959
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-25 15:07:24 +0530

    Update string_get_c.sv

--- DAY-4/STRINGS/string_get_c.sv
+++ DAY-4/STRINGS/string_get_c.sv
@@ -1,1 +1,13 @@
+module str_getchar;
+  string s1, s2, s3;
 
+  initial begin
+    s1 = "SAM";
+    s2 = "LRKS";
+    s3 = "KAR";
+    
+    $display("get character in str1: %0s", s1.getc(2));
+    $display("put character in str2: %0s", s2.getc(3));  
+    $display("put character in str3: %0s", s1.getc(1));
+  end
+endmodule
