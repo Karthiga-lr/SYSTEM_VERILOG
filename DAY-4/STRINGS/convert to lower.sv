c76910b1b8331ed6159676f34b7c1a127c648fd4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-25 15:53:23 +0530

    Update convert to lower.sv

--- DAY-4/STRINGS/convert to lower.sv
+++ DAY-4/STRINGS/convert to lower.sv
@@ -1,1 +1,11 @@
-
+module lower_case;
+   string s1,s2;
+  initial begin
+    s1 = "ART";
+    s2 = "DESIGN";
+    
+    $display("string s1:%0s", s1.tolower());
+    $display("string s2:%0s", s2.tolower());
+            
+  end   
+endmodule
