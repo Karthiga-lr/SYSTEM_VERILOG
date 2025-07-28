3020692bf66a9f75c4b3cc35481dd121ca94d4cb
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-28 18:39:31 +0530

    Update wire.sv

--- DAY-1/DATA_TYPES/4_STATE DATA_TYPES/wire.sv
+++ DAY-1/DATA_TYPES/4_STATE DATA_TYPES/wire.sv
@@ -3,10 +3,10 @@ module and_gate_example;
 
   assign a = 1'b1;
   assign b = 1'b0;
-  assign y = a & b;
+  assign y = a & b; //continuous assignment 
 
   initial begin
-    #10;
+    #10; // important to add delay to get signal updates and show values 
     $display("AND Gate: %b & %b = %b", a, b, y);
   end
 endmodule
