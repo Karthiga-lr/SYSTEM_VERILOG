4d05f4960be23ba740150b9ffe80807dd8d02fad
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-29 18:09:00 +0530

    Create randc_cyclic.sv

---
+++ DAY-5/CONSTRAINT/RANDOM VARIABLES/randc_cyclic.sv
@@ -0,0 +1,17 @@
+class random;
+  randc logic [2:0] a; //cyclic
+endclass
+
+random r1;
+module random_num;
+  initial begin
+    r1 = new();
+    $display("Random number of cyclic");
+    for(int i =0; i<=17; i++)begin
+      if(r1.randomize())
+        $display("Random number of a: %d",r1.a);
+      else
+        $display("Randomization failed");
+    end
+  end
+endmodule
