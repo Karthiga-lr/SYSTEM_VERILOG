4992ecc21759d2bc3f480ce9f35fdd987e30059d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-30 17:55:21 +0530

    Update inverted inside constraint.sv

--- DAY-5/CONSTRAINT/INSIDE CONSTRAINT/inverted inside constraint.sv
+++ DAY-5/CONSTRAINT/INSIDE CONSTRAINT/inverted inside constraint.sv
@@ -1,1 +1,17 @@
+class ins_con;
+  rand bit [3:0] value;
+  constraint c1{!(value inside {[5:10]});}
+endclass
+
+module val_range;
+  initial begin
+    ins_con t1 = new();
+ for (int i = 0; i <= 30; i++) begin
+      if(t1.randomize())
+        $display("Iteration = %0d, value = %0d", i, t1.value);
+      else
+        $display("Randomization failed at iteration %0d", i);
+    end
+  end
+endmodule
 
