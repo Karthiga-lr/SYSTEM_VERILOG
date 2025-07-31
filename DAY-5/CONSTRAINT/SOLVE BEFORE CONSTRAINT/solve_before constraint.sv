5874c239e1ddc1cf39cfaf30270c3631f71811cb
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-31 12:33:33 +0530

    Update solve_before constraint.sv

--- DAY-5/CONSTRAINT/SOLVE BEFORE CONSTRAINT/solve_before constraint.sv
+++ DAY-5/CONSTRAINT/SOLVE BEFORE CONSTRAINT/solve_before constraint.sv
@@ -1,14 +1,17 @@
 class solve_example;
   rand int a,b,c;
   constraint calc{a inside{[1:10]};
-                  b inside {[1:10]}; c == a+b;
-                  solve a before b;}
+                  b inside {[1:10]}; c == a+b;}
+                  solve a before c;}
 endclass
 module tb;
   solve_example test;
   initial begin
   test = new();
-    test.randomize();
-    $display("a = %0d b = %0d c = %0d",test.a,test.b,test.c);
+    for(int i = 0; i <=10; i++)
+      if(test.randomize())
+        $display("Iteration = %0d a = %0d b = %0d c = %0d",i,test.a,test.b,test.c);
+    else
+      $display("Randomization is failed");
   end
 endmodule
