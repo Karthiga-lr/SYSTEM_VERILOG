023c2c24b948ea3c809fd46934eafe8b118c43dc
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-31 14:46:23 +0530

    Update randomize.sv

--- DAY-6/CONSTRAINT_PART2/RANDOMIZATION METHODS/randomize.sv
+++ DAY-6/CONSTRAINT_PART2/RANDOMIZATION METHODS/randomize.sv
@@ -1,1 +1,14 @@
-
+class random_cons;
+  rand bit [3:0] range;
+  constraint c { range inside {[4:12]};}
+endclass
+module test;
+  initial begin
+    random_cons ts1 = new();
+    for (int i =0; i<=20; i++)
+        if(ts1.randomize())
+        $display("Iteration = %0d Randomize = %0d",i,ts1.range);
+    else
+      $display("Randomization is failed");
+  end
+endmodule
