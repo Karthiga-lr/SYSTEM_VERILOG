997480f639abff72c30fd7eda2f9f0ebdc3cb11e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-07-31 17:05:48 +0530

    Update pre_randomize.sv

--- DAY-6/CONSTRAINT_PART2/RANDOMIZATION METHODS/pre_randomize.sv
+++ DAY-6/CONSTRAINT_PART2/RANDOMIZATION METHODS/pre_randomize.sv
@@ -1,1 +1,26 @@
-
+class prerandom_cons;
+  rand bit [3:0] range;
+  int i;
+  
+  function void pre_randomize();
+    if(i%2 ==0)begin
+      rand_mode(0);
+      $display("Randomization is stopped");
+    end
+    else
+      rand_mode(1);
+    i++;
+  endfunction
+endclass
+module test;
+  initial begin
+    prerandom_cons ts1 = new();
+    for (int i =0; i<=5; i++)begin
+      $display("calling randomization");
+        if(ts1.randomize())
+        $display("Iteration = %0d Randomize = %0d",i,ts1.range);
+    else
+      $display("Randomization is failed");
+    end
+  end
+endmodule
