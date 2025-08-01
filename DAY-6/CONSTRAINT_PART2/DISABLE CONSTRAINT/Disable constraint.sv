488bb8c6a6e84707305f8ee61de2ea5cfcc0dc69
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-01 19:12:18 +0530

    Update Disable constraint.sv

--- DAY-6/CONSTRAINT_PART2/DISABLE CONSTRAINT/Disable constraint.sv
+++ DAY-6/CONSTRAINT_PART2/DISABLE CONSTRAINT/Disable constraint.sv
@@ -1,1 +1,22 @@
+class cons_dis;
+  rand bit [3:0] value;
+  constraint c1 {value == 5;}
+endclass
 
+module test;
+  initial begin
+    cons_dis d1;
+    d1 = new();
+    for (int i = 0;i <=5;i++)begin
+      if(d1.randomize())
+        $display("Iteration = %0d, Randomization occured at value = %0d",i,d1.value);
+    else
+      $display("Randomization is failed");
+    end
+    for (int i =0; i <=5; i++)begin
+      d1.c1.constraint_mode(0);
+      $display("Disable operation",d1.c1.constraint_mode());
+    end
+  end
+endmodule
+                       
