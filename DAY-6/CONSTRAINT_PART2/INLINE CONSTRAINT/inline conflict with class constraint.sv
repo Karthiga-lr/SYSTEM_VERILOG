71915b5ffe63e5d71916d6e58a4249046668470c
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-01 14:01:49 +0530

    Update inline conflict with class constraint.sv

--- DAY-6/CONSTRAINT_PART2/INLINE CONSTRAINT/inline conflict with class constraint.sv
+++ DAY-6/CONSTRAINT_PART2/INLINE CONSTRAINT/inline conflict with class constraint.sv
@@ -1,1 +1,17 @@
+class gen;
+  rand bit [3:0] a;
+  constraint cls1 { a == 5;}
+endclass
 
+module test;
+  initial begin
+  gen t1;
+  t1 = new();
+    for(int i=1; i<=10; i++)begin
+      if(t1.randomize() with {a ==3;})
+         $display("Randomization of a = %0d",i,t1.a);
+         else
+           $display("Randomization is failed");
+    end
+  end
+endmodule
