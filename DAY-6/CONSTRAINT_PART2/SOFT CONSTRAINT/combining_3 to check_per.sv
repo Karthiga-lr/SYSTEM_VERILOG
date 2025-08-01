44e44753c3cb10dd2f1b5ff6db934b4813f064a8
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-01 18:15:40 +0530

    Update combining_3 to check_per.sv

--- DAY-6/CONSTRAINT_PART2/SOFT CONSTRAINT/combining_3 to check_per.sv
+++ DAY-6/CONSTRAINT_PART2/SOFT CONSTRAINT/combining_3 to check_per.sv
@@ -1,1 +1,34 @@
+class cons;
+  rand bit [3:0] val;
+  constraint val1 { val > 10; }
+endclass
 
+module test;
+  initial begin
+    cons t1;
+    t1 = new();
+    
+    for (int i = 0; i < 3; i++) begin
+      if (t1.randomize() with {soft val < 10;}) //Ignores soft constraint but the normal constraint works 
+        $display("Iteration: %0d, Randomized value: %0d", i, t1.val);
+      else
+        $display("Iteration: %0d, Randomization FAILED", i);
+    end
+  
+    
+    for (int i = 0; i < 3; i++) begin
+      if (t1.randomize()) //Normal randomization
+        $display("Iteration: %0d, Randomized value: %0d", i, t1.val);
+      else
+        $display("Iteration: %0d, Randomization FAILED", i);
+    end
+   
+    
+    for (int i = 0; i < 3; i++) begin
+      if (t1.randomize() with {val < 10;}) //inline constrait
+        $display("Iteration: %0d, Randomized value: %0d", i, t1.val);
+      else
+        $display("Iteration: %0d, Randomization FAILED", i);
+    end
+  end
+endmodule
