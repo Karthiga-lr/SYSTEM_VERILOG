ce6fabe6823e5d7dec7c72a8ef665c5ee486f3b7
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-04 23:13:57 +0530

    Update int to longint.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/STATIC CASTING/int to longint.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/STATIC CASTING/int to longint.sv
@@ -1,1 +1,11 @@
-
+module static_casting_int_to_long_int;
+  int int_d;
+  longint longint_d;
+  initial begin
+    int_d = 8'b1101_z1x0;
+    $display("default size of int_d = %0b",$size(int_d));
+    $display("Before casting of int_d = %0b",int_d);
+    int_d = longint'(int_d);
+    $display("After casting int_d to longint_d = %0b",int_d);
+  end
+endmodule
