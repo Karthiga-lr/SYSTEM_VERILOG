0242cdc7f75d56d8a836e96d26d6077ee0536454
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 11:31:11 +0530

    Update real to int.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/real to int.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/real to int.sv
@@ -1,1 +1,10 @@
-
+module dynamic_real_to_int;
+  real real_d;
+  int int_d;
+  initial begin
+    real_d = 7.805;
+    $display("Before casting of real_d = %0f",real_d);
+    $cast(int_d,real_d);
+    $display("After casting of real_d into int_d = %0d",int_d);
+  end
+endmodule
