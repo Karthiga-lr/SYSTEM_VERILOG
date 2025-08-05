4fcadc7e997df237e8cfeb82be4e294c95d1e52b
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 14:08:48 +0530

    Create bit to byte.sv

---
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/STATIC CASTING/bit to byte.sv
@@ -0,0 +1,10 @@
+module static_bit_to_byte;
+  bit [3:0] bit_d;
+  byte byte_d;
+  initial begin
+    bit_d = 4'b10zx;
+    $display("Before casting the data = %0b", bit_d);
+    byte_d = byte'(bit_d);
+    $display("After casting the data = %b",byte_d);
+  end
+endmodule
