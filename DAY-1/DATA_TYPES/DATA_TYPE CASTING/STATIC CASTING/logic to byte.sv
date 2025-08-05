f8a00a00c58e41f6e5b1522bcaac021caa0fb5cc
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 12:10:53 +0530

    Create logic to byte.sv

---
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/STATIC CASTING/logic to byte.sv
@@ -0,0 +1,11 @@
+module static_logic_to_byte;
+  logic [4:0] l_data;
+  byte byte_d;
+  initial begin
+    l_data = 4'b1x0z;
+    $display("Before casting of data = %0b",l_data);
+    byte_d = byte'(l_data);
+    $display("After casting the data = %0b", byte_d);
+  end
+endmodule
+    
