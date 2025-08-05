718f62f1dcfe7fa966ecf61901749485ac0b0899
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 11:50:26 +0530

    Update real to time.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/real to time.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/real to time.sv
@@ -1,1 +1,10 @@
-
+module dynamic_real_to_time;
+  real real_d;
+  time time_d;
+  initial begin
+    real_d = 7.05;
+    $display("Before casting of real_d = %0f",real_d);
+    $cast(time_d,real_d);
+    $display("After casting of real_d into time_d = %0t",time_d);
+  end
+endmodule
