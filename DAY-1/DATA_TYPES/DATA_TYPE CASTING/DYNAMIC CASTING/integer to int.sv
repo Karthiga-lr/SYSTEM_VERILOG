5f6f401437516a8daef5958421ec84169bcab46a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 14:21:21 +0530

    Update integer to int.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/integer to int.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/integer to int.sv
@@ -2,9 +2,9 @@ module dynamic_integer_to_int;
   integer integer_d;
   int int_d;
   initial begin
-    integer_d = 8'b1011_11xz;
+    integer_d = 8'b1011_1100;
     $display("value of integer before casting = %0b",integer_d);
-    integer_d = ($cast(int_d,integer_d));
-    $display("Value of integer after casting = %0b",int_d);
+    $cast(int_d,integer_d);
+    $display("Value of integer after casting = %b",int_d);
   end
 endmodule
