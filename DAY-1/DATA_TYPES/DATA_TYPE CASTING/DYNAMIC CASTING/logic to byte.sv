a90a62f7cab2971726db301cfdf473c80ce2e922
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-05 12:22:18 +0530

    Update logic to byte.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/logic to byte.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/logic to byte.sv
@@ -1,1 +1,10 @@
-
+module dynamic_logic_to_byte;
+  logic [4:0] l_data;
+  byte byte_d;
+  initial begin
+    l_data = 4'b1x0z;
+    $display("Before casting of data = %0b",l_data);
+    byte_d = byte'(l_data);
+    $display("After casting the data = %b", byte_d);
+  end
+endmodule
