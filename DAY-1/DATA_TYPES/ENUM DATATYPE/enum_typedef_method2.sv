2ffcf5ab8ed222e7fb8127f9c60e1c2561664782
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-06 22:40:58 +0530

    Update enum_typedef_method2.sv

--- DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef_method2.sv
+++ DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef_method2.sv
@@ -1,1 +1,19 @@
-
+module type_enum;
+  typedef enum {MON,TUE,WED,THU,FRI,SAT,SUN} days;
+  days day;
+  
+  initial begin
+    day = day.first();
+    $display("First day name = %0s and it is value = %0d",day.name,day);
+    day = day.last();
+    $display("Last day name = %0s and it is value = %0d",day.name,day);
+    day = WED;
+    day = day.next();
+    $display("Next day name = %0s and it is value = %0d",day.name,day);
+    day = WED;
+    day = day.prev();
+    $display("Prev day name = %0s and it is value = %0d",day.name,day);
+    day = day.num();
+    $display("Numbers of days = %0d",day); 
+  end
+endmodule  
