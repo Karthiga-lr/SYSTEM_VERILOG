3cc409b71140346c55d0cb38aaadf07313af7214
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-06 12:56:09 +0530

    Update enum_typedef.sv

--- DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef.sv
+++ DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef.sv
@@ -1,1 +1,12 @@
-
+module type_def_enum;
+  typedef enum{JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP}month;
+  month mon1,mon2,mon3,mon4,mon5;
+  initial begin
+    mon1 = JAN;
+    mon2 = FEB;
+    mon3 = MAR;
+    mon4 = APR;
+    mon5 = MAY;
+    $display("mon1 = %0d mon2 = %0d mon3 = %0d mon4 = %0d mon5 = %0d",mon1,mon2,mon3,mon4,mon5);
+  end
+endmodule
