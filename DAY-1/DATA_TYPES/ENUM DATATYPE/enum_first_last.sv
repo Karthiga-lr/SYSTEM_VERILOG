9c5b49d1970640ef08ec5670463e42123529c080
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-06 12:25:29 +0530

    Create enum_first_last.sv

---
+++ DAY-1/DATA_TYPES/ENUM DATATYPE/enum_first_last.sv
@@ -0,0 +1,16 @@
+module default_value_enum;
+  enum{JAN,FEB,MAR,APR,MAY,JUNE,JULY,AUG,SEP}month;
+
+initial begin
+  
+  $display("JAN,\n FEB,\n MAR,\n APR,\n MAY,\n JUN,\n JULY");
+  
+  month = month.first;
+  $display("Month first = %0d",month);   
+  for(int i=0;i<7;i++) begin
+    $display("Month name =%0s  and its default value is = %0d",month.name,month);
+    month = month.next;
+  end
+  $display("Month next = %0d",month.name);
+ end
+ endmodule
