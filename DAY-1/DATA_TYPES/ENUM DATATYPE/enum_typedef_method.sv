9737d97ad8cc19a19d56e01e960a039bf905e6c1
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-06 22:13:27 +0530

    Update enum_typedef_method.sv

--- DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef_method.sv
+++ DAY-1/DATA_TYPES/ENUM DATATYPE/enum_typedef_method.sv
@@ -1,1 +1,17 @@
-
+module type_enum;
+  typedef enum {JAN,FEB,MAR,APR,MAY,JUN,JUL} month;
+  month mon;
+  
+  initial begin
+    mon = mon.first();
+    $display("First mon name = %0s and it is value = %0d",mon.name,mon);
+    mon = mon.last();
+    $display("Last mon name = %0s and it is value = %0d",mon.name,mon);
+    mon = MAR;
+    mon = mon.next();
+    $display("Next mon name = %0s and it is value = %0d",mon.name,mon);
+    mon = MAR;
+    mon = mon.prev();
+    $display("Prev mon name = %0s and it is value = %0d",mon.name,mon);
+  end
+endmodule    
