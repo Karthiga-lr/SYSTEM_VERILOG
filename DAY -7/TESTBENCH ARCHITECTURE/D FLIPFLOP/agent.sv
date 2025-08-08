99a5647eae7b5c2a195d79a0e8e8e9b4ef651bf5
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-08 11:16:44 +0530

    Update agent.sv

--- DAY -7/TESTBENCH ARCHITECTURE/D FLIPFLOP/agent.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/D FLIPFLOP/agent.sv
@@ -1,1 +1,29 @@
+class agent;
+  generator gen;
+  driver drv;
+  monitor mon;
+
+  mailbox gen2drv;
+  mailbox mon2scb;
+  
+  virtual intf vif;
+
+  function new(virtual intf v, mailbox m2s);
+    vif = v;
+    mon2scb = m2s;
+    gen2drv = new();
+
+    gen = new(gen2drv);
+    drv = new(vif, gen2drv);
+    mon = new(vif, mon2scb);
+  endfunction
+
+  task run();
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+    join
+  endtask
+endclass
 
