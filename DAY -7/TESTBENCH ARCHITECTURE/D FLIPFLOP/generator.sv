cb676ed1021a3f279441f43b348be383281a17a7
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-08 11:12:19 +0530

    Create generator.sv

---
+++ DAY -7/TESTBENCH ARCHITECTURE/D FLIPFLOP/generator.sv
@@ -0,0 +1,20 @@
+class generator;
+  trans tr;  //transaction instancename
+  mailbox gen2drv;
+ 
+  function new(mailbox m);
+    gen2drv = m;
+  endfunction
+  task main();
+    int i;
+    for (i=0;i<10;i++)begin
+      tr = new();
+      if(!tr.randomize())begin
+        $display("Randomization is failed");
+      end else begin
+        tr.display("Generator class signal");
+        gen2drv.put(tr);
+      end
+    end
+  endtask
+endclass
