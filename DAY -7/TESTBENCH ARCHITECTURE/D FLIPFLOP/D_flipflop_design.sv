e6b2771b03e3ca32736710c8c420b06679f84c36
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-08 11:11:29 +0530

    Update D_flipflop_design.sv

--- DAY -7/TESTBENCH ARCHITECTURE/D FLIPFLOP/D_flipflop_design.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/D FLIPFLOP/D_flipflop_design.sv
@@ -1,1 +1,13 @@
-
+module dff(clk,rst,d,q);
+  input clk;
+  input rst;
+  input d;
+  output reg q;
+  always @(posedge clk)
+    begin
+    if(rst)
+      q<= 1'b0;
+      else
+      q <=d;
+    end
+endmodule
