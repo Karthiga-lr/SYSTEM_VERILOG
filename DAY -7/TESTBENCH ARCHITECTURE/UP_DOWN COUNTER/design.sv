688f94006edcf801ab28eea76d2bac89106ab7a7
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-11 22:43:50 +0530

    Update design.sv

--- DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/design.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/design.sv
@@ -1,1 +1,14 @@
-
+module up_down_counter(
+  input logic clk,
+  input logic rst,
+  input logic d,
+  output reg [3:0] count);
+  always@(posedge clk or posedge rst)begin
+    if(rst)
+      count <= 4'b0;
+  else if(d)
+    count <= count+1;
+  else
+    count <= count-1;
+  end
+endmodule
