852ec80b078e77feb3436abba53a6a090a3c6d88
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-11 22:44:46 +0530

    Update testbench.sv

--- DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/testbench.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/testbench.sv
@@ -1,1 +1,17 @@
+`include "interface.sv"
+`include "test.sv"
 
+module testbench;
+  intf intf_if();
+  test tst(intf_if);
+  
+  initial begin
+    intf_if.clk = 0;
+    forever #5 intf_if.clk = ~intf_if.clk;
+    //200;
+    //$finish;
+  end
+  
+  up_down_counter dut (.clk(intf_if.clk),.rst(intf_if.rst),.d(intf_if.d),.count(intf_if.count));
+  
+endmodule
