bb9dc446a80498299294d4523d9a5bf109362501
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-11 22:51:57 +0530

    Update agent.sv

--- DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/agent.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/UP_DOWN COUNTER/agent.sv
@@ -1,1 +1,32 @@
+class agent;
+  generator gen;
+  driver drv;
+  monitor mon;
+
+  mailbox gen2drv;
+  mailbox mon2scb;
+  event scb2gen;
+  
+  virtual intf vif;
+
+  function new(virtual intf v, mailbox m2s, event s);
+    vif = v;
+    mon2scb = m2s;
+    scb2gen = s;
+    gen2drv = new();
+
+    gen = new(gen2drv,s);
+    drv = new(vif, gen2drv);
+    mon = new(vif, mon2scb);
+    
+  endfunction
+
+  task run();
+    fork
+      gen.main();
+      drv.main();
+      mon.main();
+    join
+  endtask
+endclass
 
