23a2a91a6d47f04eb63a3ba07f0d1b8aca98246a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-12 13:31:25 +0530

    Create transaction.sv

---
+++ DAY -7/TESTBENCH ARCHITECTURE/SRAM/transaction.sv
@@ -0,0 +1,14 @@
+class transaction #(parameter addr = 3, parameter data = 4);
+  bit clk;
+  bit rst;
+  rand bit wr;
+  randc bit [addr -1:0] addrs;
+  rand bit [data -1:0]din;
+  bit [data -1:0] dout;
+  
+  function void display(string st);
+    $display("%s",st);
+    $display("clk = %b, rst = %b, wr = %b, addrs = %b, din = %b, dout= %b", clk,rst,wr,addrs,din,dout);
+  endfunction
+endclass
+          
