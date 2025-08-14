27fb84e8cce6da2edca3aa1ed30587a08b39dd91
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-14 14:05:11 +0530

    Update generator.sv

--- DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/generator.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/generator.sv
@@ -1,1 +1,22 @@
+class generator;
+  transaction tr;
+  mailbox gen2drv;
+  event scb2gen;
+  function new(mailbox m,event s);
+    gen2drv = m;
+    scb2gen = s;
+  endfunction
+  task main();
+    tr = new();
+    for(int i = 0; i<=10; i++)begin
+      if(!tr.randomize())
+        $display("Randomization is failed");
+    else 
+      tr.display("Generator Class Signal");
+      gen2drv.put(tr);
+    end
+    @scb2gen;
+  endtask
+endclass
+        
 
