b72397c8d5a199b66976db95492ffd902b2a1ff1
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-14 14:11:21 +0530

    Update environment.sv

--- DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/environment.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/environment.sv
@@ -1,1 +1,24 @@
+`include "agent.sv";
+`include "scorebaord.sv";
 
+class environment;
+  agent agt;
+  scoreboard scv;
+  mailbox mon2scb;
+  event s;
+  
+  function new(virtual intf vif);
+    mon2scb = new();
+    agt = new(vif,mon2scb,s);
+    scb = new(mon2sb,scb2gen);
+    
+  endfunction
+  
+  task run();
+    fork
+      agt.run();
+      scb.run();
+    join
+  endtask
+endclass
+      
