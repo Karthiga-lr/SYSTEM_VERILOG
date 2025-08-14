ad8a6a898c04e5b852be75dafc8115554a00902e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-14 14:07:19 +0530

    Update agent.sv

--- DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/agent.sv
+++ DAY -7/TESTBENCH ARCHITECTURE/SYNCHRONOUS FIFO/agent.sv
@@ -1,1 +1,37 @@
+`include "transaction.sv";
+`include "generator.sv";
+`include "driver.sv";
+`include "monitor.sv";
 
+class agent;
+  generator gen;
+  driver drv;
+  monitor mon;
+  
+  mailbox gen2drv;
+  mailbox mon2scb;
+  event scb2gen;
+  virtual intf vif;
+  
+  function new(virtual inf v, mailbox m2s,event s);
+    vif = v;
+    mon2scb = m2s;
+    scb2gen = s;
+    gen2drv = new();
+    
+    gen = new(gen2drv,scb2gen);
+    drv = new(vif,gen2drv);
+    mon = new(vif,mon2scb);
+    
+  endfunction
+  
+  task run();
+    fork 
+    gen.main();
+    drv.main();
+    mon.main();
+    join
+  endtask
+endclass
+    
+  
