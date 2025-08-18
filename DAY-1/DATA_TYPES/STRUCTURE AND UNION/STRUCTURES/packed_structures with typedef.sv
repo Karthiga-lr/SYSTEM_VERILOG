1285817b5c3b48a5cddb867dd40b601cd02ba65a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-18 14:39:33 +0530

    Update packed_structures with typedef.sv

--- DAY-1/DATA_TYPES/STRUCTURE AND UNION/STRUCTURES/packed_structures with typedef.sv
+++ DAY-1/DATA_TYPES/STRUCTURE AND UNION/STRUCTURES/packed_structures with typedef.sv
@@ -1,1 +1,12 @@
-
+module packed_typedef;
+  typedef struct{
+    bit [15:0] id;
+    logic [15:0]contact_num;
+    integer reference_num;
+  } person_details;
+  person_details per_dt;
+  initial begin
+    per_dt ='{101,25255,2554};
+    $display("Person details = %0p",per_dt);
+  end
+endmodule
