0e0a20f292485315ab4efcba138b9d98e1a95075
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-18 15:33:50 +0530

    Update unpacked_union.sv

--- DAY-1/DATA_TYPES/STRUCTURE AND UNION/UNION/unpacked_union.sv
+++ DAY-1/DATA_TYPES/STRUCTURE AND UNION/UNION/unpacked_union.sv
@@ -1,1 +1,16 @@
-
+module unpacked_union;
+  union{
+  bit result;
+  byte grade;
+  int marks;
+  } student_details;
+  
+  initial begin
+    student_details.result = 1;
+    student_details.grade = 4;
+    student_details.marks = 95;
+    
+    $display("Student details = %0p", student_details);  
+   
+  end
+endmodule
