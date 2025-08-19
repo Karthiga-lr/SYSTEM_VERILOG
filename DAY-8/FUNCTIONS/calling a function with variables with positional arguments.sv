e93347ccec68dfd540de768391b04699cf1d5755
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-19 15:15:58 +0530

    Update calling a function with variables with positional arguments.sv

--- DAY-8/FUNCTIONS/calling a function with variables with positional arguments.sv
+++ DAY-8/FUNCTIONS/calling a function with variables with positional arguments.sv
@@ -1,1 +1,13 @@
-
+module func_with_posi;
+  int result;
+  
+  initial begin
+    result = mult(.var1(10), .var2(5));
+    $display("result = %0d",result);
+  end
+  
+  function int mult(int var1, var2);
+    $display("Inside function var1 = %0d and var2 = %0d",var1,var2);
+    return var1*var2;
+  endfunction 
+endmodule
