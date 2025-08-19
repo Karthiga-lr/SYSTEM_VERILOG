9de892e48e0ea128ecea266ce85184a39cb88206
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-19 11:50:46 +0530

    Update calling a function with values as arguments.sv

--- DAY-8/FUNCTIONS/calling a function with values as arguments.sv
+++ DAY-8/FUNCTIONS/calling a function with values as arguments.sv
@@ -1,1 +1,15 @@
-
+module func_call_value;
+  int result;
+  
+  initial begin
+    $display("Calling the function");
+    result = square(7);
+    $display("Returned from function");
+    $display("REsult = %0d",result);
+  end
+  
+  function int square(int x);
+    $display("Inside the function which is received: x = %0d",x);
+    return x*x;
+  endfunction
+endmodule   
