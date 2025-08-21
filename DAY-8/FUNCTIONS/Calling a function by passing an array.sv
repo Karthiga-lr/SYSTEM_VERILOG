ecbe2e30338cf0c70d48f2575d10094245a4b5a0
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-21 19:15:27 +0530

    Update Calling a function by passing an array.sv

--- DAY-8/FUNCTIONS/Calling a function by passing an array.sv
+++ DAY-8/FUNCTIONS/Calling a function by passing an array.sv
@@ -1,1 +1,14 @@
-
+module array_tb;
+  function automatic void increment(ref int x);
+    $display("Inside function before increment: x = %0d",x);
+    x=x+1;
+    $display("Inside function after increment: x = %0d",x);
+  endfunction
+  
+  initial begin
+    int a = 10;
+    $display("Before calling function: a = %0d",a);
+    increment(a);
+    $display("After calling function: a = %0d",a);
+  end
+endmodule
