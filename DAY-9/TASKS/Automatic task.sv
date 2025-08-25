f8479735c8980170ea6521fbe9f46df835e104ee
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-25 15:33:00 +0530

    Update Automatic task.sv

--- DAY-9/TASKS/Automatic task.sv
+++ DAY-9/TASKS/Automatic task.sv
@@ -1,1 +1,18 @@
-
+module fact_task;
+  
+int result;
+  initial begin
+  factorial(5,result);
+    $display("Factorial(5) = %0d",result);
+  end
+  
+  task automatic factorial(input int n, output int result);
+    int temp;
+    if(n<=1)
+      result =1;
+    else begin
+      factorial (n-1,temp);
+      result = n*temp;
+    end
+  endtask
+endmodule  
