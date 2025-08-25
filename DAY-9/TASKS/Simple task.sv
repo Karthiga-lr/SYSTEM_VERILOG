7bd161359d5ac32b528ac0938665c475fd07349a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-25 14:49:38 +0530

    Update Simple task.sv

--- DAY-9/TASKS/Simple task.sv
+++ DAY-9/TASKS/Simple task.sv
@@ -1,1 +1,12 @@
-
+module simple_task;
+  int num = 26, den = 5, q,r;
+  initial begin
+    divide(num,den,q,r);
+    $display("%0d/ %0d Quotient = %0d, Reminder = %0d", num,den,q,r);
+  end
+  
+  task divide(input int a,b, output int quotient,reminder);
+    quotient = a/b;
+    reminder = a%b;
+  endtask
+endmodule
