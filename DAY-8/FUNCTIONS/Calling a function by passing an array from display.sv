24420f875762a300113c356234ad5568c64ebd23
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-25 11:58:42 +0530

    Update Calling a function by passing an array from display.sv

--- DAY-8/FUNCTIONS/Calling a function by passing an array from display.sv
+++ DAY-8/FUNCTIONS/Calling a function by passing an array from display.sv
@@ -1,1 +1,15 @@
-// TO DO
+module test;
+  typedef int array_lst[10];   //typedef 
+  array_lst hd1;               //handle (storage for return)
+  
+  function array_lst fillarray(int arr[10]);
+    foreach (arr[i])
+      arr[i] = (i+1)*10;
+    return arr;
+  endfunction
+  
+  initial begin
+    $display("Function by an array from display");
+    $display("Returned array = %0p", fillarray(hd1));
+  end
+endmodule
