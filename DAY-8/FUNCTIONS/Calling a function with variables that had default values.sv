7d72ef1a784d08d066a4ca23ef4ff964a08c54e3
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-25 12:12:33 +0530

    Update Calling a function with variables that had default values.sv

--- DAY-8/FUNCTIONS/Calling a function with variables that had default values.sv
+++ DAY-8/FUNCTIONS/Calling a function with variables that had default values.sv
@@ -1,1 +1,16 @@
-
+module default_val;
+  
+  function int sum(int a = 5, b= 15); // default values
+    return a + b;
+  endfunction
+  int result;
+  initial begin
+    $display("Calling a function with default values");
+    result = sum();
+    $display("Sum = %0d",result);
+    result = sum(10);
+    $display("sum = %0d",result); //overides a
+    result = sum(20,40);
+    $display("sum = %0d",result); //overides both a and b
+  end
+endmodule
