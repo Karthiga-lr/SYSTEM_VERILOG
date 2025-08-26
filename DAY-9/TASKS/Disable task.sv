ff8d5e4b441c1b5e0e2949a35d14d5a992d60cd4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-26 22:59:36 +0530

    Update Disable task.sv

--- DAY-9/TASKS/Disable task.sv
+++ DAY-9/TASKS/Disable task.sv
@@ -1,1 +1,23 @@
-//
+module disable_threads;
+  initial begin
+    fork
+      run_task();
+      #15 disable run_task.t1;
+    join
+  end
+
+
+task run_task();
+  begin:t1
+    $display("@0t ns: Thread 1 started", $time);
+    #30;
+    $display("@0t ns: Thread 1 finished", $time);
+  end:t1
+  
+  begin:t2
+    $display("@0t ns: Thread 2 started", $time);
+    #10;
+    $display("@0t ns: Thread 2 finished", $time);
+  end:t2
+endtask
+endmodule
