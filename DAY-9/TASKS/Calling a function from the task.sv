b32f17b0e02a83793b12f227fdf34372c8377101
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-26 23:32:49 +0530

    Update Calling a function from the task.sv

--- DAY-9/TASKS/Calling a function from the task.sv
+++ DAY-9/TASKS/Calling a function from the task.sv
@@ -1,2 +1,18 @@
-//
-
+module func_from_task_exe;
+  initial begin
+    check_number(7);
+    check_number(20);
+  end
+  
+  task check_number(input int n);
+    #1;
+    if(is_even(n))
+      $display("@0t ns :%0d is EVEN",$time,n);
+    else
+      $display("@0t ns :%0d is ODD",$time,n);
+  endtask
+  
+  function bit is_even(int x);
+    return(x % 2 == 0);
+  endfunction
+endmodule
