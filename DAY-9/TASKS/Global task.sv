789ea93bc4a86764d6d72cbee5247619c3625cb9
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-26 22:39:25 +0530

    Update Global task.sv

--- DAY-9/TASKS/Global task.sv
+++ DAY-9/TASKS/Global task.sv
@@ -1,1 +1,20 @@
+task add(input int a,b, output int res);
+  res = a+b;
+endtask
 
+module m1;
+  int r;
+  initial begin
+    add(5,5,r);
+    $display("Add: 5+5 = %0d",r);
+  end
+endmodule
+
+module m2;
+  int r;
+  initial begin
+    #5ns;
+    add(10,20,r);
+    $display("Add: 10+20 = %0d",r);
+  end
+endmodule
