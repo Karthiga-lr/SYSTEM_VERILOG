1353b952f34ea77db614343ddf5b091310849c9d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-27 18:13:22 +0530

    Update class.sv

--- DAY-10/CLASSES/class.sv
+++ DAY-10/CLASSES/class.sv
@@ -1,1 +1,13 @@
-
+module tb;
+  class student;
+  static bit result; // static is used here to call the class without creating object
+  static int marks;
+  static task progress_report;
+    result = 1;
+    $display("Progress report(result) of student : %0d pass",result);
+  endtask
+  endclass
+  initial begin
+   student:: progress_report();
+  end
+endmodule
