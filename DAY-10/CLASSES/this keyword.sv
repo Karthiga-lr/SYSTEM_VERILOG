3ab6c578fbcb37d2d7e5c26e85a26ad7510a2e0a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-27 22:07:39 +0530

    Update this keyword.sv

--- DAY-10/CLASSES/this keyword.sv
+++ DAY-10/CLASSES/this keyword.sv
@@ -1,1 +1,17 @@
-
+class student;
+  string name;
+  int marks;
+  
+  function new(string name, int marks);
+    this.name = name; //update class property(this.name)
+    this.marks = marks; // this.marks - class property
+  endfunction
+endclass
+  
+module tb;
+    initial begin
+    student s1;
+    s1 = new("sam",95);
+    $display("Student name = %0s, Student marks = %0d",s1.name,s1.marks);
+    end
+endmodule 
