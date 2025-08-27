c6aa3c212a8ba75e9c2d7765e1799c329423468e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-27 18:52:03 +0530

    Update Class object.sv

--- DAY-10/CLASSES/Class object.sv
+++ DAY-10/CLASSES/Class object.sv
@@ -1,1 +1,19 @@
-
+class student;
+  bit result; 
+  int marks;
+  task progress_report;
+    result = 1;
+    $display("Progress report(result) of student : %0d pass",result);
+  endtask
+endclass
+module tb;
+  initial begin
+  student s1; // declare handle
+    s1 = new(); //object created
+  if(s1==null)
+    $display("The student is absent for exam");
+    else 
+     $display("The student is present for exam"); 
+    s1.progress_report();  // call task
+   end  
+endmodule
