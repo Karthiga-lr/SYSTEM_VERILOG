b5b8baff920d1fd4bea540e887239f39d81f1a4e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-28 14:38:46 +0530

    Update Extern keyword.sv

--- DAY-10/CLASSES/Extern keyword.sv
+++ DAY-10/CLASSES/Extern keyword.sv
@@ -1,1 +1,16 @@
-/
+class student;
+  static int marks = 85;  // class property 
+  extern task mark_result(int m); 
+endclass
+  
+task student::mark_result(int m);
+  marks = m;   // updates the class variable
+endtask
+  
+module tb;
+  initial begin
+    $display("student marks = %0d", student::marks);
+    //student::mark_result(95);
+    //$display("Updated student marks = %0d",student::marks);
+  end
+endmodule
