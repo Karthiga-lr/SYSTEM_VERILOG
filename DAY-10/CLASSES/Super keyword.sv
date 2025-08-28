e75bc1333978bdb7ac214379cb2534b469b7b92c
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-28 12:36:07 +0530

    Update Super keyword.sv

--- DAY-10/CLASSES/Super keyword.sv
+++ DAY-10/CLASSES/Super keyword.sv
@@ -1,1 +1,22 @@
+class student;
+  string name = "sam";
+  function void display();
+  $display("Name = %0s",name);
+  endfunction
+endclass
 
+class student_members extends student;
+  string name = "Anu"; //overrides parent property
+  function void display();
+    super.display();   //calls sam
+    $display("Name = %0s",name);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    student_members s1;
+    s1 = new();
+    s1.display();
+  end
+endmodule 
