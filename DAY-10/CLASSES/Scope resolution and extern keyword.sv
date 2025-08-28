6a26fe0f9c101f1d3c52f011951776adc7cbe85d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-28 15:02:26 +0530

    Update Scope resolution and extern keyword.sv

--- DAY-10/CLASSES/Scope resolution and extern keyword.sv
+++ DAY-10/CLASSES/Scope resolution and extern keyword.sv
@@ -1,1 +1,14 @@
-
+class student;
+  int marks = 85;  // class property 
+  extern function void display; 
+endclass
+function void student::display();
+  $display("student marks = %0d",marks);
+endfunction
+  
+module tb;
+  initial begin
+    student s1 = new();
+    s1.display();
+  end
+endmodule
