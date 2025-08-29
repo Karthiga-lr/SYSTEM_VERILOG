8c9b79f243494679d73d03f6a7279e0cec1d2df3
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-29 11:19:26 +0530

    Update Instant constants.sv

--- DAY-11/CLASSES_PART_2/Instant constants.sv
+++ DAY-11/CLASSES_PART_2/Instant constants.sv
@@ -1,1 +1,23 @@
+class student;
+  const int roll_no;
+  string name;
 
+function new(int r, string n);
+  roll_no = r;
+  name = n;
+endfunction
+
+function void display();
+  $display("Roll no = %0d, Name = %0s",roll_no,name);
+endfunction
+endclass
+
+module tb;
+  initial begin
+    student s1 = new(102,"LATHA");
+    student s2 = new(105,"ANU");
+    s1.display();
+    s2.display();
+    // s3.roll_no = 202; Error
+  end
+endmodule
