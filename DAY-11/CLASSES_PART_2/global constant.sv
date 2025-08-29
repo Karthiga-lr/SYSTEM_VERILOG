9dae78b73c69d54cc5fa6503f5d44a9b16f000ad
Karthiga-lr <lr.karthiga20@gmail.com>
2025-08-29 10:38:39 +0530

    Update global constant.sv

--- DAY-11/CLASSES_PART_2/global constant.sv
+++ DAY-11/CLASSES_PART_2/global constant.sv
@@ -1,1 +1,21 @@
+class vehicle;
+  const int wheels = 4;
+  string model;
 
+function new(string m);
+  model = m;
+endfunction
+
+function void display();
+  $display("model = %0s, wheels = %0d",model,wheels);
+endfunction
+endclass
+
+module tb;
+  initial begin
+    vehicle m1 = new("Tesla");
+    vehicle m2 = new("KIA");
+    m1.display();
+    m2.display();
+  end
+endmodule
