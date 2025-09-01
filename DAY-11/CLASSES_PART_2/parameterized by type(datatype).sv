68b6e267eb18705adfde57d0f930605419e775f0
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-01 15:19:28 +0530

    Update parameterized by type(datatype).sv

--- DAY-11/CLASSES_PART_2/parameterized by type(datatype).sv
+++ DAY-11/CLASSES_PART_2/parameterized by type(datatype).sv
@@ -1,1 +1,25 @@
-//
+class box #(parameter type T = int);
+  T item;
+  function new(T value);
+    item = value;
+  endfunction
+  
+  function void display();
+    $display("Box contains = %p",item);
+  endfunction
+endclass
+module tb;
+    initial begin
+      box#(int) b1;
+      box#(string) b2;
+      box#(bit[7:0]) b3;
+      
+      b1 = new(85);
+      b2 = new("SAM");
+      b3 = new(8'hAC);
+    
+    b1.display();
+    b2.display();
+    b3.display();
+    end
+endmodule
