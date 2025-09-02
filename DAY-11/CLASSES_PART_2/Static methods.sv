fb96a636ac29e7cdf3901b034011ed6b65c4bd96
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-02 14:39:25 +0530

    Update Static methods.sv

--- DAY-11/CLASSES_PART_2/Static methods.sv
+++ DAY-11/CLASSES_PART_2/Static methods.sv
@@ -1,2 +1,20 @@
-//
+class counter;
+  static int object_count = 5;
+  
+  static function void increment();
+    object_count++;
+  endfunction
+  
+  static function void show_count();
+    $display("Total object created = %0d",object_count);
+  endfunction
+endclass
 
+module tb;
+  initial begin
+    counter::increment();
+    counter::increment();
+    counter::show_count();
+    
+  end
+endmodule
