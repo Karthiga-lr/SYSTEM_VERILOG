9734d343cb491b06ea975e28be8b0bb1b80698a8
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-02 22:20:40 +0530

    Update inheritance.sv

--- DAY-11/CLASSES_PART_2/inheritance.sv
+++ DAY-11/CLASSES_PART_2/inheritance.sv
@@ -1,1 +1,19 @@
-//TO-DO
+class teacher;
+  function void subject();
+    $display("student chooses subject");
+  endfunction 
+endclass
+
+class student extends teacher;
+  function void section();
+    $display("Teacher seperates section");
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    student s = new();
+    s.subject();
+    s.section();
+  end
+endmodule
