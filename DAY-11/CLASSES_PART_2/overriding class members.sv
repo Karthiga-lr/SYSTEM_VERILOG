9b3e837ec129e2ce643d7d949aa99019c96f2abd
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-02 13:15:51 +0530

    Update overriding class members.sv

--- DAY-11/CLASSES_PART_2/overriding class members.sv
+++ DAY-11/CLASSES_PART_2/overriding class members.sv
@@ -1,1 +1,26 @@
-/// TO-DO
+class vehicle;
+  int wheels = 4;
+  function void display();
+    $display("Vehicle has %0d wheels",wheels);
+  endfunction
+endclass
+
+class Bike extends vehicle;
+  int wheels = 2;
+  function void display();
+    $display("Bike has %0d wheels", wheels);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    vehicle v = new();
+    Bike b = new(); 
+    v.display();
+    b.display();
+    
+    b.wheels = 3;
+    v.display();
+    b.display();
+  end
+endmodule
