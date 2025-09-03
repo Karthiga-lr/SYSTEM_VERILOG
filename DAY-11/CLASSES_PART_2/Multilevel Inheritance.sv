dc5649903a6cbff8ebd67f84b967a032903af96e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-03 23:10:49 +0530

    Update Multilevel Inheritance.sv

--- DAY-11/CLASSES_PART_2/Multilevel Inheritance.sv
+++ DAY-11/CLASSES_PART_2/Multilevel Inheritance.sv
@@ -1,1 +1,25 @@
+class grandparent;
+  function void display();
+    $display("grandparent present");
+  endfunction
+endclass
 
+class parent extends grandparent;
+  function void display();
+    $display("parent present");
+  endfunction
+endclass
+
+class child extends parent;
+  function void display();
+    super.display(); // Calls parent.display()
+    $display("child present");
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    child c = new();
+    c.display(); // This triggers parent and child display functions
+  end
+endmodule
