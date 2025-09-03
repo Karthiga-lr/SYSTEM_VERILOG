0367908ecdcb053fe897e497c1e47cdafbd4c48d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-03 23:50:50 +0530

    Update Polymorphism.sv

--- DAY-11/CLASSES_PART_2/Polymorphism.sv
+++ DAY-11/CLASSES_PART_2/Polymorphism.sv
@@ -1,1 +1,34 @@
-to do
+// Code your design here
+class shape;
+  virtual function void display();
+    $display("generate shape");
+    endfunction 
+endclass
+
+class circle extends shape;
+  function void display();
+    $display("Drawing a circle");
+  endfunction 
+endclass
+
+class square extends shape;
+  function new();
+    endfunction 
+  function void display();
+    $display("Drawing a square");
+    endfunction 
+endclass
+
+module tb;
+  initial begin
+    shape s;
+    circle c;
+    square sq;
+    c = new();
+    s = c;
+    s.display();
+    sq = new();
+    s = sq;
+    s.display();
+  end
+endmodule
