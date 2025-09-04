331091d6226a8a931e1c0ee3a1f57a37834b7e07
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-04 19:14:15 +0530

    Update abstract or virtual class.sv

--- DAY-12/OOPS/ABSTRACT OR VIRTUAL CLASS/abstract or virtual class.sv
+++ DAY-12/OOPS/ABSTRACT OR VIRTUAL CLASS/abstract or virtual class.sv
@@ -1,1 +1,40 @@
-// To _Do
+virtual class Animal;
+  string name;
+
+  function new(string n);
+    name = n;
+  endfunction
+
+  function void display();
+    $display("%s makes a sound", name);
+  endfunction
+endclass
+
+class Dog extends Animal;
+  function new(string n);
+    super.new(n);
+  endfunction
+
+  function void display();
+    $display("%s barks: Woof!", name);
+  endfunction
+endclass
+
+class Cat extends Animal;
+  function new(string n);
+    super.new(n);
+  endfunction   // No override
+endclass
+
+module tb1;
+  initial begin
+    Animal a;   // abstract handle
+    Dog d = new("Tommy");
+    Cat c = new("Kitty");
+
+    a = d; 
+    a.display();  // calls Dog’s version
+    a = c; 
+    a.display();  // calls parent’s version (since Cat didn’t override)
+  end
+endmodule
