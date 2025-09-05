4090e3af4870b4f8b4e2a6bf15c25fad4162878d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-05 12:36:38 +0530

    Update encapsulation_local.sv

--- DAY-12/OOPS/ENCAPSULATION/encapsulation_local.sv
+++ DAY-12/OOPS/ENCAPSULATION/encapsulation_local.sv
@@ -1,1 +1,24 @@
-
+class simple_loc_encaps;
+  int a;
+  local int b;   //local (only inside the class)
+  
+  function void set_values(int x,int y);
+    a = x;
+    b = y;
+  endfunction
+  
+  function void show();
+    $display("Values of a = %0d, b = %0d",a,b);
+  endfunction
+endclass
+  
+  module tb;
+    initial begin
+    simple_loc_encaps s;
+    s = new();
+    s.set_values(10,30);
+    s.show();
+    s.a = 20;
+    s.show();
+    end
+  endmodule
