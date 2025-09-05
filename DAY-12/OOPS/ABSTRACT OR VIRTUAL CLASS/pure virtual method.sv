73e1c5be18d511829e489a74e345d15e52cd9efb
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-05 10:29:25 +0530

    Create pure virtual method.sv

---
+++ DAY-12/OOPS/ABSTRACT OR VIRTUAL CLASS/pure virtual method.sv
@@ -0,0 +1,32 @@
+virtual class shape;
+  pure virtual function void draw(); 
+endclass
+
+class square extends shape;
+  function void draw();
+    $display("Drawing a square");
+    endfunction 
+endclass
+
+class circle extends shape;
+  function void draw();
+    $display("Drawing a circle");
+    endfunction 
+endclass
+
+module tb;
+  initial begin 
+    shape s;
+    square sq;
+   circle c;
+    
+    sq = new();
+    
+    s = sq;
+    s.draw();
+    c = new();
+    s = c;
+    s.draw();
+  end
+  endmodule 
+  
