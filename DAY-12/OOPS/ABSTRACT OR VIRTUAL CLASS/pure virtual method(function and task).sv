2467a3d14c97966abeb59e240349ffd4da66ee9e
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-05 11:07:09 +0530

    Update pure virtual method(function and task).sv

--- DAY-12/OOPS/ABSTRACT OR VIRTUAL CLASS/pure virtual method(function and task).sv
+++ DAY-12/OOPS/ABSTRACT OR VIRTUAL CLASS/pure virtual method(function and task).sv
@@ -1,1 +1,24 @@
-
+virtual class A;
+  int a,b,c;
+  pure virtual function void display();
+  pure virtual task sum();
+endclass
+class B extends A;
+  virtual function void display();
+    a = 10;
+    $display("Value of a = %0d, b = %0d. c= %0d",a,b,c);
+  endfunction
+  virtual task sum();
+    c = a+b;
+    $display("Value of a=%0d, b=%0d, c=%0d",a,b,c);
+  endtask
+endclass
+  module tb;
+    B b1;
+    initial begin
+      b1 = new();
+      b1.display();
+      b1.b = 20;
+      b1.sum();
+    end
+  endmodule
