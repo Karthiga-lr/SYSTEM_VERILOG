c9176f04191ed4595653ba04e7e50bb74877f83a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-08 23:36:43 +0530

    Update constructor in child class method.sv

--- DAY-12/OOPS/INHERITANCE/constructor in child class method.sv
+++ DAY-12/OOPS/INHERITANCE/constructor in child class method.sv
@@ -1,1 +1,26 @@
-//
+class parent;
+  int a;
+  function new(int x);
+    a = x;
+  endfunction
+endclass
+
+class child extends parent;
+  int b;
+  function new(int x, int y);
+    super.new(x);
+    b = y;
+  endfunction
+  
+  function void show();
+    $display("a = %0d, b = %0d",a,b);
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    child c;
+    c = new(10,20);
+    c.show();
+  end
+endmodule
