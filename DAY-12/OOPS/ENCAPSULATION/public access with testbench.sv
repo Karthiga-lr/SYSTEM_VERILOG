ebfc66ab03793e1a29577c7455e32573c9b25b6a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-08 18:28:33 +0530

    Update public access with testbench.sv

--- DAY-12/OOPS/ENCAPSULATION/public access with testbench.sv
+++ DAY-12/OOPS/ENCAPSULATION/public access with testbench.sv
@@ -1,1 +1,19 @@
-//
+class account;
+  string holder;
+  int balance;
+  
+  function new(string h, int b);
+    holder = h;
+    balance = b;
+  endfunction
+endclass
+
+module tb;
+  initial begin
+    account a1;
+    a1 = new("Anu",3000);
+    $display("Holder = %0s, Balance = %0d",a1.holder,a1.balance);
+    a1.balance -= 500;
+    $display("updated balance account = %0d",a1.balance);
+  end
+endmodule
