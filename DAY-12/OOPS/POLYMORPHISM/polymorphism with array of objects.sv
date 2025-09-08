38ebb0e38f1bab88902bce343e86fbf4f305f260
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-08 23:55:23 +0530

    Update polymorphism with array of objects.sv

--- DAY-12/OOPS/POLYMORPHISM/polymorphism with array of objects.sv
+++ DAY-12/OOPS/POLYMORPHISM/polymorphism with array of objects.sv
@@ -1,1 +1,32 @@
-//
+class vehicle;
+  virtual function void run();
+    $display("Vehicle is approaching");
+  endfunction
+endclass
+class car extends vehicle;
+  function void run();
+    $display("Car is running on the road");
+  endfunction
+endclass
+class bike extends vehicle;
+  function void run();
+    $display("Bike is running on the track");
+  endfunction
+endclass
+
+module tb;
+  vehicle v[$];
+  int i;
+  initial begin
+    car c;
+    bike b;
+    c = new();
+    b = new();
+    v.push_back(c);
+    v.push_back(b);
+  
+    foreach (v[i]) begin
+      v[i].run();
+    end
+  end
+endmodule
