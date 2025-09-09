d0a4f90a171c1a602e44319853ef09f92afc6423
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-09 22:11:26 +0530

    Update fork_join_any.sv

--- DAY-13/PROCESSES/PROCESS OR THREADS/fork_join_any.sv
+++ DAY-13/PROCESSES/PROCESS OR THREADS/fork_join_any.sv
@@ -1,1 +1,15 @@
-
+module tb_fork_join_any;
+  initial begin
+    $display("[%0t] parent starts",$time);
+    fork 
+      begin
+        #5 $display("[%0t] child1 done",$time);
+      end
+      begin
+        #15 $display("[%0t] child2 done", $time);
+      end
+    join_any
+    $display("[%0t] parent continuous after any child done",$time);
+    #20;
+  end
+endmodule
