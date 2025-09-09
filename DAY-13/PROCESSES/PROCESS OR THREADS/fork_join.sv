40af285d07c23351b2cf97b2ae1290630a4814a3
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-09 17:59:18 +0530

    Update fork_join.sv

--- DAY-13/PROCESSES/PROCESS OR THREADS/fork_join.sv
+++ DAY-13/PROCESSES/PROCESS OR THREADS/fork_join.sv
@@ -1,1 +1,11 @@
-
+module tb;
+  initial begin
+    $display("[%0t] parent class", $time);
+    fork
+      #5 $display("[%0t] child 1: Finished after 5 time units",$time);
+      #15 $display("[%0t] child 1: Finished after 15 time units",$time);
+      #8 $display("[%0t] child 1: Finished after 7 time units",$time);
+    join
+    $display("[0t] parents resumes until all children finishes",$time);
+  end
+endmodule
