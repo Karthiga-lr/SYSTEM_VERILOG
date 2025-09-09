8060aa2e641102148b998fb43c5b106d6eaaf499
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-09 13:19:50 +0530

    Update deep_copy.sv

--- DAY-11/CLASSES_PART_2/deep_copy.sv
+++ DAY-11/CLASSES_PART_2/deep_copy.sv
@@ -2,7 +2,7 @@ class student;
   string name;
   int roll_no;
   
-  function new(string n="AB", int r=5);
+  function new(string n= "a", int r=8);
     name = n;
     roll_no = r;
   endfunction
@@ -21,14 +21,17 @@ module tb;
   initial begin
   student s1,s2;
   s1 = new("ANU",125);
-  s2 = new();
+    s2 = new();
+  
+    s2.display();
+    
   s2.deep(s1);
   $display("Before changes");
   s1.display();
   s2.display();
   
-  s1.name = "RADHA";
-  s1.roll_no = 150;
+  s1.name = "KA";
+  s2.roll_no = 150;
   $display("After changes");
   s1.display();
   s2.display();
