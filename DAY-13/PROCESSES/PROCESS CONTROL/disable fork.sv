7ecaab545ee438019cdd5e518c00d9581708296a
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-10 12:24:44 +0530

    Update disable fork.sv

--- DAY-13/PROCESSES/PROCESS CONTROL/disable fork.sv
+++ DAY-13/PROCESSES/PROCESS CONTROL/disable fork.sv
@@ -1,1 +1,18 @@
-//
+module tb_disable_fork;
+  initial begin
+    $display("[%0t] parent started",$time);
+    fork
+      begin
+        #5 $display("[%0t] child1 finished", $time);
+      end
+      begin
+        #15 $display("[%0t] child2 finished",$time);
+      end
+      begin
+        #25 $display("[%0t] child3 finished",$time);
+      end
+    join_any
+    disable fork;
+      $display("[%0t] parent after disable fork", $time);
+      end
+      endmodule
