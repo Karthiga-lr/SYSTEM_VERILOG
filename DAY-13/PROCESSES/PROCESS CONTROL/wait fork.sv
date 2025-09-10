0251713633ce0be94d6ae8c1af71bd85858a8af4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-10 11:50:14 +0530

    Update wait fork.sv

--- DAY-13/PROCESSES/PROCESS CONTROL/wait fork.sv
+++ DAY-13/PROCESSES/PROCESS CONTROL/wait fork.sv
@@ -1,1 +1,17 @@
-
+module tb_disable_fork;
+  initial begin
+    $display("[%0t] parent started", $time);
+    fork 
+      begin
+        #5 $display("[%0t] child1 done",$time);
+      end
+      begin
+        #20 $display("[%0t] child2 done",$time);
+      end
+    join_none
+    
+    $display("[%0t] parent continuous immediately",$time);
+    wait fork;
+      $display("[%0t] All child process completely",$time);
+      end
+      endmodule
