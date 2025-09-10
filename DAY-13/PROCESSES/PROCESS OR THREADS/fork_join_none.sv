0ddcfeb5bff3bd6e8a63bd986cdfa65a8acec555
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-10 10:40:51 +0530

    Update fork_join_none.sv

--- DAY-13/PROCESSES/PROCESS OR THREADS/fork_join_none.sv
+++ DAY-13/PROCESSES/PROCESS OR THREADS/fork_join_none.sv
@@ -1,1 +1,15 @@
-
+module tb_fork_join_none;
+  initial begin
+    $display("[%0t] parent starts",$time);
+    fork 
+      begin
+        #5 $display("[%0t] child1 none",$time);
+      end
+      begin
+      #15 $display("[%0t] child2 none", $time);
+      end
+    join_none
+    $display("[%0t] parent doesn't until the chils finishes",$time);
+    #20;
+  end
+endmodule
