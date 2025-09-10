3933b65ef5f99758c0dc0d7f97a54ec3b8f2fa8b
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-10 22:51:38 +0530

    Update status.sv

--- DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/status.sv
+++ DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/status.sv
@@ -1,1 +1,18 @@
-
+module ex2;
+  process p1;
+  event e;
+  
+  initial begin
+    fork 
+      begin
+        p1 = process::self();
+        $display("[%0t] p1 waiting for e",$time);
+        wait(e.triggered);
+        $display("[%0t] p1 resumed, status = %s", $time,p1.status());
+      end
+    join_none
+    
+    #2 -> e;
+    #1 $display("[%0t] outside: status = %s",$time, p1.status());
+  end
+endmodule
