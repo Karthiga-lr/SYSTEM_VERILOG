f3aa7d8ad23dda5db13349c783fdc4c9318828a4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-10 23:14:42 +0530

    Update await.sv

--- DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/await.sv
+++ DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/await.sv
@@ -1,1 +1,20 @@
-
+/*module await_example;
+  process p1,p2;
+ 
+  initial begin
+    fork
+      begin
+        p1 = process::self();
+        #5;
+        $display("[%0t] process 1 finished",$time);
+      end
+      
+      begin
+        p2 = process::self();
+        $display("[%0t] p2 waiting for p1",$time);
+        p2.await(p1);
+        $display("p2 resumes after p1 is finished",$time,);
+      end
+    join
+  end
+endmodule*/  error
