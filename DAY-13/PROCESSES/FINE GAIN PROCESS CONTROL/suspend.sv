9c332e7ec5850f66f09a8d8d531a86a5a08c49bf
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-11 12:37:08 +0530

    Update suspend.sv

--- DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/suspend.sv
+++ DAY-13/PROCESSES/FINE GAIN PROCESS CONTROL/suspend.sv
@@ -1,1 +1,21 @@
-
+module tb;
+  process p1,p2;
+  initial begin
+    fork
+      begin
+        p1 = process::self();
+        $display("[%0t] process 1 is running",$time);
+        #10;
+        p1.suspend();
+        $display("[%0t] process 1 is suspended",$time);
+      end
+      
+      begin
+        p2 = process::self();
+        $display("[%0t] process 2 is running",$time);
+        #15;
+        $display("[%0t] process 2 is finished",$time);
+      end
+    join
+  end
+endmodule
