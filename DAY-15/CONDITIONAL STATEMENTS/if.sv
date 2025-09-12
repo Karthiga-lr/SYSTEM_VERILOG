86696302f207d82c125c627199d2e2da9452b45b
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-12 15:36:56 +0530

    Update if.sv

--- DAY-15/CONDITIONAL STATEMENTS/if.sv
+++ DAY-15/CONDITIONAL STATEMENTS/if.sv
@@ -1,1 +1,12 @@
-
+module conditional_if_code;
+  bit[4:0] a;
+  initial begin
+    a = 31;
+    $display("The value of a: %0b",a);
+    if(a == 32)begin
+      $display("It works here if the if-condition is true");
+    $display("a is eqaul to 10");
+  end
+  $display("Out of block");
+  end
+endmodule
