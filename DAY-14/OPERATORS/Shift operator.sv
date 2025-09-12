bb36184da25ab985e187cb3adf6b65ac3aaaf484
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-12 14:18:23 +0530

    Update Shift operator.sv

--- DAY-14/OPERATORS/Shift operator.sv
+++ DAY-14/OPERATORS/Shift operator.sv
@@ -1,1 +1,12 @@
-
+module shift_operator;
+  logic [3:0] a;
+  
+  initial begin
+    a = 3'b101;
+    $display("The value of a: %0d",a);
+    $display("Logical left shift of a: %0b", a<<1);
+    $display("Logical right shift of a: %0b", a>>1);
+    $display("Logical arithmetic left shift of a: %0b", a<<<1);
+    $display("Logical arithmetic right shift of a: %0b", a>>>1);
+  end
+endmodule
