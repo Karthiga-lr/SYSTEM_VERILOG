646077e9c941cff768dd3d9154e3818062dd1116
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-15 13:12:05 +0530

    Create continue.sv

---
+++ DAY-15/CONDITIONAL STATEMENTS/continue.sv
@@ -0,0 +1,12 @@
+module continue_tb;
+  int array[5] = {1,2,3,4,5};
+  initial begin
+    for (int i = 0; i<5; i++)begin
+      if(i == 2)begin
+        continue;
+        $display("Iteration = %0d",i);
+      end
+      $display("Iteration = %0d",i);
+      end
+  end
+endmodule
