2c1e78f80fe2f2b6499dcd001b59df8d336fb03d
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-16 12:16:14 +0530

    Update Calling a function with void return type.sv

--- DAY-8/FUNCTIONS/Calling a function with void return type.sv
+++ DAY-8/FUNCTIONS/Calling a function with void return type.sv
@@ -1,11 +1,10 @@
 module void_d;
   initial begin
     $display("Hello World");
+    display("This is my function call");
   end
   
   function void display(string str);
     $display("%s",str);
   endfunction
 endmodule
-
-//another program
