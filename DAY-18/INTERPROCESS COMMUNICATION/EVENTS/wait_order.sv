d038a428663ca3bc3e3cf145c7e9cb9574d84ac4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-17 22:59:39 +0530

    Update wait_order.sv

--- DAY-18/INTERPROCESS COMMUNICATION/EVENTS/wait_order.sv
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/wait_order.sv
@@ -1,1 +1,15 @@
+module waitorder;
+  event e1,e2,e3;
+  
+  initial begin
+    $display("%0t Waiting for e1, e2, e3 in order", $time);
+    wait_order(e1, e2, e3);
+    $display("%0t Correct order observed!", $time);
+  end
 
+  initial begin
+    #5 ->e1;
+    #10 ->e2;
+    #15 ->e3;
+  end
+endmodule
