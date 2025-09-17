ce7cbb79b15f1a72839b2b2022d659a2f838bb2f
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-17 23:05:09 +0530

    Create wait_order(not happened).sv

---
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/wait_order(not happened).sv
@@ -0,0 +1,15 @@
+module waitorder;
+  event e1,e2,e3;
+  
+  initial begin
+    $display("%0t Waiting for e1, e2, e3 in order", $time);
+    wait_order(e1, e2, e3);
+    $display("%0t Correct order observed!", $time);
+  end
+
+  initial begin
+    #5 ->e2;
+    #10 ->e1;
+    #15 ->e3;
+  end
+endmodule
