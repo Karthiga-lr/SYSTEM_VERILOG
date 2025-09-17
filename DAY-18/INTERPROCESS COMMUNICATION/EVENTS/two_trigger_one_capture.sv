5648ad0e9260054445d58abea1156813793f8e91
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-17 22:10:55 +0530

    Create two_trigger_one_capture.sv

---
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/two_trigger_one_capture.sv
@@ -0,0 +1,21 @@
+module event_eg;
+  event e;
+
+  initial begin
+    $display($time,"Triggering the event");
+    #4 ->e;
+    #3 ->e;  
+  end
+
+  initial begin
+    $display($time," Waiting using wait");
+    #2wait(e.triggered); 
+    $display($time, " Event triggered (wait)");
+  end
+
+ initial begin
+   $display($time," Waiting using @");
+   #1 @(e); 
+   $display($time," First Event triggered (@)");
+  end
+endmodule
