52b6e0697a5d02987fa70d6296fd898f91495297
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-18 23:49:01 +0530

    Update two_non_blocking.event.sv

--- DAY-18/INTERPROCESS COMMUNICATION/EVENTS/two_non_blocking.event.sv
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/two_non_blocking.event.sv
@@ -1,1 +1,20 @@
-
+module non_block;
+  event e;
+  
+  initial begin
+    #5->> e;
+    $display("%0t Non_Blocking trigger done",$time);
+  end
+  
+  initial begin
+    #10->> e;
+    $display("%0t Non_Blocking trigger done",$time);
+  end
+  
+  initial begin
+    @(e.triggered)
+    $display("%0t Event seen with @",$time);
+    @(e.triggered)
+    $display("%0t Event seen again with @",$time);
+  end
+endmodule
