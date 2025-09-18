a26324d27f135251d756466a07640f5f2c46f440
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-18 23:22:51 +0530

    Update Blocking_event.sv

--- DAY-18/INTERPROCESS COMMUNICATION/EVENTS/Blocking_event.sv
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/Blocking_event.sv
@@ -1,1 +1,17 @@
-
+module blocking_event;
+  event BJT;
+  initial begin 
+    $display("%0t triggering the event",$time);
+    ->BJT;
+  end 
+  initial begin
+    $display("%0t waiting for the event using wait",$time);
+    wait(BJT.triggered);
+    $display("%0t event BJT is triggered using wait",$time);
+  end
+  initial begin
+    $display("%0t waiting for the event using @",$time);
+    @(BJT.triggered);
+    $display("%0t event BJT is triggered using @",$time);
+  end
+endmodule
