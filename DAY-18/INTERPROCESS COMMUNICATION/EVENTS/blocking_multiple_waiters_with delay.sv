f30afe8fac06cd7c7d4993227d46ab6ff62165b4
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-18 23:59:58 +0530

    Update blocking_multiple_waiters_with delay.sv

--- DAY-18/INTERPROCESS COMMUNICATION/EVENTS/blocking_multiple_waiters_with delay.sv
+++ DAY-18/INTERPROCESS COMMUNICATION/EVENTS/blocking_multiple_waiters_with delay.sv
@@ -1,1 +1,22 @@
+module block_multiple_waiters;
+  event ev;
+  initial begin
+    #5->ev; 
+    $display("%0t Event triggered", $time);
+  end
 
+  initial begin
+    @(ev.triggered);
+    $display("%0t Process 1 saw event", $time);
+  end
+
+  initial begin
+    @(ev.triggered);
+    $display("%0t Process 2 saw event", $time);
+  end
+
+  initial begin
+    wait(ev.triggered);
+    $display("%0t Process 3 saw event (wait)", $time);
+  end
+endmodule
