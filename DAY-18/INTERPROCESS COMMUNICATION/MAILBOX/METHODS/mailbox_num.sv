0891b3c5cb050feb612783a41848f79996ea8e80
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-23 23:34:14 +0530

    Create mailbox_num.sv

---
+++ DAY-18/INTERPROCESS COMMUNICATION/MAILBOX/METHODS/mailbox_num.sv
@@ -0,0 +1,36 @@
+class A;
+  int a;
+  int i;
+  mailbox m;
+  
+  function new(mailbox m1);
+    this.m = m1;
+  endfunction
+  
+  task check();
+    
+    if(m==null)
+      $display("Mailbox is not create");
+    else
+      $display("Mailbox is created");
+    for(i =0; i<5; i++)begin
+      a++;
+          $display("value of a = %0d",a);
+      end
+      m.put(a);
+    $display("value of a_put = %0d",a);
+    $display("Number of mailbox messages = %0d",m.num());
+  endtask
+endclass
+
+module tb;
+  A a1;
+  mailbox mb = new(); 
+ 
+  initial begin
+    a1 = new(mb);
+    repeat(5)begin 
+      a1.check();
+    end
+  end 
+endmodule 
