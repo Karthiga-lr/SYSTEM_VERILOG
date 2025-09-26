dd81d490654960e697876185402f889b6dc1c949
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-26 11:13:41 +0530

    Update int to longint.sv

--- DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/int to longint.sv
+++ DAY-1/DATA_TYPES/DATA_TYPE CASTING/DYNAMIC CASTING/int to longint.sv
@@ -1,1 +1,10 @@
-
+module dynamic_int_to_longint;
+  longint longint_d;
+  int int_d;
+  initial begin
+    int_d = 15678;
+    $display("value of longint before casting = %0b",longint_d);
+    $cast(longint_d,int_d);
+    $display("Value of longint after casting = %h",longint_d);
+  end
+endmodule
