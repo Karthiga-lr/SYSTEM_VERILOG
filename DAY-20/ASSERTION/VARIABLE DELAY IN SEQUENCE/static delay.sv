c81e0a6900cb5c469348d92117afd31a1939e8f1
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-26 22:09:08 +0530

    Create static delay.sv

---
+++ DAY-20/ASSERTION/VARIABLE DELAY IN SEQUENCE/static delay.sv
@@ -0,0 +1,27 @@
+module static_delay;
+  bit clk,a,b;
+  
+  always #5 clk = ~clk;
+  
+  initial begin
+    clk = 0;
+    a = 0; b = 0;
+    #10 a = 1; b = 1;
+    #10 a = 0; b = 1;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 1;
+    #10 a = 0; b = 0;
+    #20 $finish;
+  end
+  
+  property p;
+    @(posedge clk) a|-> ##3 b;
+  endproperty
+  
+  assert property(p)
+    $display("pass at time %0t",$time);
+    else
+    $display("Fail at time %0t",$time); 
+    endmodule
+      
+  
