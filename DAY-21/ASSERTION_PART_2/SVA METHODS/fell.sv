a2bb1d1899b433727b75d9931b252d7522b38592
Karthiga-lr <lr.karthiga20@gmail.com>
2025-09-29 15:29:11 +0530

    Update fell.sv

--- DAY-21/ASSERTION_PART_2/SVA METHODS/fell.sv
+++ DAY-21/ASSERTION_PART_2/SVA METHODS/fell.sv
@@ -1,1 +1,34 @@
-
+module fell;
+  bit clk,a,b;
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    a = 0; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 1;
+    #10 a = 0; b = 0;
+    #10 a = 1; b = 0;
+    #10 a = 0; b = 1;
+    #10 a = 1; b = 0;
+    #10 $finish;
+  end
+  
+  property p;
+    @(posedge clk) a-> $fell(b);
+  endproperty
+  
+  assert property (p)
+    $info("pass at time %0t",$time);
+    else
+      $info("Fail at time %0t", $time);
+    
+    initial begin
+      $dumpfile("test.vcd");
+      $dumpvars();
+    end
+    endmodule
