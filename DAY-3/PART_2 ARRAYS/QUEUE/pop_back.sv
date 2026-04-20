0b19001002ab779c2c1230b3eee468f082d29bc3
Karthiga-lr <lr.karthiga20@gmail.com>
2026-04-20 19:34:36 +0530

    Update pop_back.sv

--- DAY-3/PART_2 ARRAYS/QUEUE/pop_back.sv
+++ DAY-3/PART_2 ARRAYS/QUEUE/pop_back.sv
@@ -1,1 +1,11 @@
-//
+module pop_back;
+  int q[$];
+  int q1[$:5];
+  
+  initial begin
+    q = {4,5,8,9};
+    q1 = {8,4,3,9,5,0,1};
+    $display("pop back q = %0p",q.pop_back());
+    $display("pop back q1 = %0p",q1.pop_back());
+  end
+  endmodule
